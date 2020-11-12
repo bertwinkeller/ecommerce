7a61443146841f103836b71470876dad9445e4a6
Bert Keller <bertwinkeller@gmail.com>
2020-11-11 19:09:19 -0800

    installed concurrently

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,12 +1,21 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap'
 import Rating from '../components/Rating'
-import products from '../products'
+import axios from 'axios'
 
 const ProductScreen = ({ match }) => {
 
-    const product = products.find(p => p._id === match.params.id)
+    const [product, setProduct] = useState({})
+
+    useEffect(() => {
+        const fetchProduct = async () => {
+            const { data } = await axios.get(`/api/products/${match.params.id}`)
+
+            setProduct(data)
+        }
+        fetchProduct()
+    })
 
 
 
@@ -59,7 +68,7 @@ const ProductScreen = ({ match }) => {
                             </ListGroup.Item>
 
                             <ListGroup.Item>
-                                <Button className='btn-btn' type='button' disable={product.countInStock === 0}>
+                                <Button className='btn-btn' type='button' disabled={product.countInStock === 0}>
                                     Add To Card
                                 </Button>
                             </ListGroup.Item>
